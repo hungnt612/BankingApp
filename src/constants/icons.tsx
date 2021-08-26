06a91094cfefd287a7d4af0d58deec2c33eb9c88
hungnt612 <hungnt612@gmail.com>
2021-08-26 23:53:38 +0700

    update assets

---
+++ src/constants/icons.tsx
@@ -0,0 +1,33 @@
+const bookmark = require('../assets/icons/bookmark.png');
+const bookmarkFilled = require('../assets/icons/bookmark-filled.png');
+const home = require('../assets/icons/home.png');
+const search = require('../assets/icons/search.png');
+const settings = require('../assets/icons/settings.png');
+const back = require('../assets/icons/back.png');
+const rightArrow = require('../assets/icons/right-arrow.png');
+
+const paypal = require('../assets/icons/paypal.png');
+const infinity = require('../assets/icons/infinity.png');
+const infinity2 = require('../assets/icons/infinity2.png');
+const user = require('../assets/icons/user.png');
+const chat = require('../assets/icons/chat.png');
+const heart = require('../assets/icons/heart.png');
+const infinity3 = require('../assets/icons/infinity3.png');
+
+export default {
+  bookmark,
+  bookmarkFilled,
+  back,
+  home,
+  search,
+  settings,
+  rightArrow,
+
+  paypal,
+  infinity,
+  infinity2,
+  user,
+  chat,
+  heart,
+  infinity3,
+};
